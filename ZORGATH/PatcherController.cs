142759323f3ba62e6d4f7bad3bf487fa9209d104
Denis Koroskin <2korden@gmail.com>
2023-05-12 22:12:50 -0700

    Add patcher.php stub that tricks the game into thinking it's up-to-date

---
+++ ZORGATH/PatcherController.cs
@@ -0,0 +1,17 @@
+﻿namespace ZORGATH;
+
+[ApiController]
+[Route("patcher/patcher.php")]
+[Consumes("application/x-www-form-urlencoded")]
+public class PatcherController : ControllerBase
+{
+    [HttpPost(Name = "Patcher")]
+    public IActionResult Patcher()
+    {
+        // Any OK response that doesn't contain a "version" field being set is treated as "up-to-date" response.
+        // This will suppress an update prompt on the client since we always want updates to go through the launcher.
+        // Note: we still want to tell the manager (but not individual game server instances) when there is a new
+        // version available, but it's not handled here at the moment.
+        return Ok(PHP.Serialize(new object()));
+    }
+}
