ae7bbedcab1a1df38a390f27a799817779a03baf
tarre <tarre.islam@gmail.com>
2023-11-15 14:33:45 +0100

    unused account check commented out, needs to be discussed

--- ZORGATH/GetProductsHandler.cs
+++ ZORGATH/GetProductsHandler.cs
@@ -10,18 +10,18 @@ public class GetProductsHandler: IClientRequesterHandler
     {
         _upgradeRepository = upgradeRepository;
     }
-    public async Task<IActionResult> HandleRequest(ControllerContext controllerContext, Dictionary<string, string> formData)
+    public Task<IActionResult> HandleRequest(ControllerContext controllerContext, Dictionary<string, string> formData)
     {
         // maybe even now introduce a repository for accounts, these 3 lines will be called many times. Also, why even do this check here? We should protect with middlewares instead. That will also keep the handlers nice and tidy
-        using BountyContext bountyContext = controllerContext.HttpContext.RequestServices.GetRequiredService<BountyContext>();
-        Account? account = await bountyContext.Accounts.FirstOrDefaultAsync(a => a.Cookie == formData["cookie"]);
-        if (account is null)
-        {
-            // return new UnauthorizedResult();
-        }
+        // using BountyContext bountyContext = controllerContext.HttpContext.RequestServices.GetRequiredService<BountyContext>();
+        // Account? account = await bountyContext.Accounts.FirstOrDefaultAsync(a => a.Cookie == formData["cookie"]);
+        //if (account is null)
+        //{
+        //     return new UnauthorizedResult();
+        //}
         
         var res = _upgradeRepository.GetProductsForClient();
         
-        return new OkObjectResult(PhpSerialization.Serialize(res));
+        return Task.FromResult<IActionResult>(new OkObjectResult(PhpSerialization.Serialize(res)));
     }
 }
